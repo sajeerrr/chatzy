3d128dfd69c140450ce4d90ba470e5f7057f3ea2
Sajeer <sajeerfm93@gmail.com>
2026-01-31 23:27:19 +0530

    chatroom and message models complete

--- accounts/models.py
+++ accounts/models.py
@@ -6,4 +6,17 @@ User = settings.AUTH_USER_MODEL
 class ChatRoom(models.Model):
     name = models.CharField(max_length=255, blank=True)
     participants = models.ManyToManyField(Users, related_name="rooms")
-    is_group
+    is_group = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name if self.name else "Private Chat"
+
+class Message(models.Model):
+    room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content[:30]    
