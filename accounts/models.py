3432f47d10143f56a6fd6f14b2a0a371114430ba
Sajeer <sajeerfm93@gmail.com>
2026-01-31 19:12:05 +0530

    account model changes

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
+User = settings.AUTH_USER_MODEL
+
+class ChatRoom(models.Model):
+    name = models.CharField(max_length=255, blank=True)
+    participants = models.ManyToManyField(Users, related_name="rooms")
+    is_group
